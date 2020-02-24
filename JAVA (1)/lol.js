9a00cbbf53b5e165db6954a45da01890ef04312e
Shahjahan226 <57897795+Shahjahan226@users.noreply.github.com>
2020-02-24 18:06:22 +0600

    Add files via upload

---
+++ JAVA (1)/lol.js
@@ -0,0 +1,27 @@
+    var myHeading= document.querySelector('h1');
+
+
+    var myHeading= document.querySelector('h1');
+    var number1= 10;
+    var number2= 15 ;
+    var string1= "И КРАСАВЧИК";
+    var daIliNet= true;
+    var list= [1, "fds", "4to", 15];
+    var summa= number1+number2;
+    myHeading.textContent= string1;
+
+    if (summa === 25) {
+        myHeading.textContent= "да";
+    } else{
+        myHeading.textContent= "нет";
+    }
+
+    function  umnoshit(num1,num2){
+        var otvet = num1*num2;
+         (otvet)
+    }
+    umnoshit(7,7)
+
+    document.querySelector('html').onclick = function () {
+      alert('ТЫ ДУРАК?');
+    }
